459e8f3514c7585a7562f9f2aa346d235a056593
@Chinedu <chinedufidelis321@gmail.com>
2024-11-01 01:23:06 +0100

    Add files via upload

---
+++ django_project/myproject/myproject/views.py
@@ -0,0 +1,16 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Hello World! I'm Home")
+    return render(request, 'home.html')
+
+def about(request):
+    # return HttpResponse("My About Page")
+    return render(request, 'about.html') 
+
+def posts_list(request):
+    return render(request, 'posts/posts_list.html')
+
+def contact(request):
+    return render(request, 'contact.html')
---
+++ django_project/myproject/static/js/main.js
@@ -0,0 +1,1 @@
+console.log("This is JS fro my About Page.")
